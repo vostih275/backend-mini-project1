02683d79a862e115912b9911b9d6c94594ec1a54
vostih275 <stevemaxwell732@gmail.com>
2026-02-09 12:14:03 +0300

    Setup simple blog page backend with posts.json and routes/posts.js

--- src/app.js
+++ src/app.js
@@ -3,22 +3,37 @@ const path = require("path");
 
 const app = express();
 
-// View engine setup
+// ----------------------
+// View Engine Setup
+// ----------------------
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// ----------------------
 // Middleware
+// ----------------------
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Static files
+// Serve static files
 app.use(express.static(path.join(__dirname, "../public")));
 
-// Home route (temporary)
+// ----------------------
+// Routes
+// ----------------------
+
+// Home route
 app.get("/", (req, res) => {
   res.send("Simple Blog Page API is running");
 });
 
+// Import posts routes
+const postsRoutes = require("./routes/posts"); // make sure this file exists
+app.use("/posts", postsRoutes);
+
+// ----------------------
+// Start Server
+// ----------------------
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
